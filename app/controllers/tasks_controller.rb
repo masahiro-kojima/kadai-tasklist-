8ae390c1ad21ef37ea5c61ad3889079cb81c02df
masahiro.kojima <mncc0220@gmail.com>
2020-07-03 09:24:33 +0000

    kadai-tasklist_2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -56,7 +56,8 @@ class TasksController < ApplicationController
 
   # Strong Parameter
   def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:content, :status)
   end
+
 
 end
